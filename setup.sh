9e1910b2a8aeaa3a711736b6c9a2a73684c7292b
Jenkins Devops <jenkins@Jenkinss.local>
2023-01-30 13:44:09 +0700

    update da

--- setup.sh
+++ setup.sh
@@ -2023,18 +2023,18 @@ if [ ! -f "/etc/sysconfig/network-scripts/ifcfg-${INET}100" ]; then
 	/usr/bin/perl -pi -e "s/^ethernet_dev=.*/ethernet_dev=${INET}:100/" /usr/local/directadmin/conf/directadmin.conf
 fi
 
-# cat << "EOF" | sudo tee /usr/local/directadmin/scripts/update-license.sh
-# 	#/bin/bash
-# 	/usr/sbin/ifdown $INET:100
-# 	rm -f /tmp/license.key
-# 	rm -f /usr/local/directadmin/conf/license.key
-# 	/usr/bin/wget -O /tmp/license.key.gz https://github.com/damvt/directadmin/raw/main/license.key.gz
-# 	/usr/bin/gunzip /tmp/license.key.gz
-# 	mv /tmp/license.key /usr/local/directadmin/conf/
-# 	chmod 600 /usr/local/directadmin/conf/license.key
-# 	chown diradmin:diradmin /usr/local/directadmin/conf/license.key
-# 	/usr/sbin/ifup $INET:100
-# 	/bin/systemctl restart directadmin.service
-# 	sleep 1;
-# 	/usr/sbin/ifdown $INET:100
-# EOF
+cat << EOF > /usr/local/directadmin/scripts/update-license.sh
+	#/bin/bash
+	/usr/sbin/ifdown $INET:100
+	rm -f /tmp/license.key
+	rm -f /usr/local/directadmin/conf/license.key
+	/usr/bin/wget -O /tmp/license.key.gz https://github.com/damvt/directadmin/raw/main/license.key.gz
+	/usr/bin/gunzip /tmp/license.key.gz
+	mv /tmp/license.key /usr/local/directadmin/conf/
+	chmod 600 /usr/local/directadmin/conf/license.key
+	chown diradmin:diradmin /usr/local/directadmin/conf/license.key
+	/usr/sbin/ifup $INET:100
+	/bin/systemctl restart directadmin.service
+	sleep 1;
+	/usr/sbin/ifdown $INET:100
+EOF
